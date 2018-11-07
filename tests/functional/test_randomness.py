dd49b2e8a8f3f93e7d77e02c149874b4b89c4993
hpesonen <henri.e.pesonen@gmail.com>
2018-11-07 08:52:23 +0200

    Test with modified test_randomness

--- tests/functional/test_randomness.py
+++ tests/functional/test_randomness.py
@@ -37,20 +37,20 @@ def test_global_random_state_usage(simple_model):
     k2.generate(n_gen)
     st1 = np.random.get_state()
 
-    # np.random.seed(0)
-    # mu = ss.uniform.rvs(0, 10, size=(n_gen, 1))
-    # ss.norm.rvs(mu, size=(n_gen, 3))
-    # st2 = np.random.get_state()
+    np.random.seed(0)
+    mu = ss.uniform.rvs(0, 10, size=(n_gen, 1))
+    ss.norm.rvs(mu, size=(n_gen, 3))
+    st2 = np.random.get_state()
 
-    # assert random_state_equal(st1, st2)
+    assert random_state_equal(st1, st2)
 
 
-# def test_get_sub_seed():
-#     n = 100
-#     seed = np.random.randint(2**31)
-#     sub_seeds = []
-#     for i in range(n):
-#         sub_seeds.append(get_sub_seed(seed, i, n))
+def test_get_sub_seed():
+    n = 100
+    seed = np.random.randint(2**31)
+    sub_seeds = []
+    for i in range(n):
+        sub_seeds.append(get_sub_seed(seed, i, n))
 
 #     assert len(np.unique(sub_seeds)) == n
 
