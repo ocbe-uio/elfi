12953542fda2ed49526efd2a56f14f6109a39c0e
hpesonen <henri.e.pesonen@gmail.com>
2018-11-12 18:19:21 +0200

    Test with modified test_randomness

--- tests/functional/test_randomness.py
+++ tests/functional/test_randomness.py
@@ -17,13 +17,13 @@ def test_randomness(simple_model):
 
 
 @pytest.mark.usefixtures('with_all_clients')
-def test_randomness2(simple_model0):
-    k1 = simple_model0['k1']
+def test_randomness2(simple_model):
+    k1 = simple_model['k1']
 
     n = 30
     # mymodel = elfi.Rejection(simple_model0['k1'], batch_size=3)
     # samples1 = elfi.Rejection(simple_model0['k1'], batch_size=3).sample(n).samples['k1']
-    rej = elfi.Rejection(simple_model0['k1'], batch_size=3)
+    rej = elfi.Rejection(simple_model['k1'], batch_size=3)
     # sample = rej.sample(n)
     # samples1 = sample.samples['k1']
     # assert len(np.unique(samples1)) == n
