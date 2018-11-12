f130ff4a63c037faab01bb06f474281400492c82
hpesonen <henri.e.pesonen@gmail.com>
2018-11-12 11:36:55 +0200

    Test with modified test_randomness

--- tests/functional/test_randomness.py
+++ tests/functional/test_randomness.py
@@ -3,7 +3,7 @@ import pytest
 import scipy.stats as ss
 
 import elfi
-from elfi.methods.parameter_inference import Rejection
+# from elfi.methods.parameter_inference import Rejection
 from elfi.utils import get_sub_seed
 
 
@@ -16,15 +16,15 @@ def test_randomness(simple_model):
     assert not np.array_equal(gen1, gen2)
 
 
-@pytest.mark.usefixtures('with_all_clients')
+# @pytest.mark.usefixtures('with_all_clients')
 def test_randomness2(simple_model):
     k1 = simple_model['k1']
 
     n = 30
-    samples1 = Rejection(simple_model['k1'], batch_size=3).sample(n).samples['k1']
+    samples1 = elfi.Rejection(simple_model['k1'], batch_size=3).sample(n).samples['k1']
     assert len(np.unique(samples1)) == n
 
-    samples2 = Rejection(simple_model['k1'], batch_size=3).sample(n).samples['k1']
+    samples2 = elfi.Rejection(simple_model['k1'], batch_size=3).sample(n).samples['k1']
     assert not np.array_equal(samples1, samples2)
 
 
