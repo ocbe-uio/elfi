41889579aae96118b9802f4e3b3778f159c7f1f4
hpesonen <henri.e.pesonen@gmail.com>
2018-11-12 20:41:07 +0200

    Test with modified test_randomness

--- tests/functional/test_randomness.py
+++ tests/functional/test_randomness.py
@@ -22,11 +22,11 @@ def test_randomness2(simple_model):
 
     n = 30
     # mymodel = elfi.Rejection(simple_model0['k1'], batch_size=3)
-    # samples1 = elfi.Rejection(simple_model0['k1'], batch_size=3).sample(n).samples['k1']
-    rej = elfi.Rejection(simple_model['k1'], batch_size=3)
-    sample = rej.sample(n_samples=n, quantile=1)
+    samples1 = elfi.Rejection(simple_model0['k1'], batch_size=100).sample(n_samples=n).samples['k1']
+    # rej = elfi.Rejection(simple_model['k1'], batch_size=3)
+    # sample = rej.sample(n_samples=n, quantile=1)
     # samples1 = sample.samples['k1']
-    # assert len(np.unique(samples1)) == n
+    assert len(np.unique(samples1)) == n
 
     # samples2 = elfi.Rejection(simple_model['k1'], batch_size=3).sample(n).samples['k1']
     # samples2 = mymodel.sample(n).samples['k1']
